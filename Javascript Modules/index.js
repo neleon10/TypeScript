f009cb3404e69deb7a4fcdab91ac60bb1376c9cf
neleon10 <cvn45@hotmail.com>
2023-05-18 20:39:26 +0200

    updating

--- Javascript Modules/index.js
+++ Javascript Modules/index.js
@@ -3,7 +3,8 @@
 // el EXPORT respeta el nombre si o si de la funcion
 // El export default no necesariamente, son canales separados. 
 import funcioncita, { area,circumference } from "./circle.mjs"; //ya no usa el require
-
+import * as path from "path"; //Aqui importo todo
 console.log(area(5.0));
 console.log(funcioncita());
+
 
